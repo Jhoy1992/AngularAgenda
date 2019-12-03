e92d8364ccbf0bb642aa839eb85350f0f7282e6e
Jhoy1992 <jhonatanascari@gmail.com>
2019-12-03 02:18:56 -0300

    feat: create services

--- js/controllers/listaTelefonicaController.js
+++ js/controllers/listaTelefonicaController.js
@@ -1,55 +1,58 @@
 angular
   .module("listaTelefonica")
-  .controller("listaTelefonicaController", ($scope, $http) => {
-    $scope.app = "Lista Telefônica"
-    $scope.contatos = []
-    $scope.operadoras = []
+  .controller(
+    "listaTelefonicaController",
+    ($scope, contatosApi, operadorasApi, serialGenerator) => {
+      $scope.app = "Lista Telefônica"
+      $scope.contatos = []
+      $scope.operadoras = []
 
-    const baseURL = "http://localhost:3001"
-
-    const carregarContatos = () => {
-      $http.get(`${baseURL}/contatos`).then(response => {
-        $scope.contatos = response.data
-      })
-    }
-
-    const carregarOperadoras = () => {
-      $http.get(`${baseURL}/operadoras`).then(response => {
-        $scope.operadoras = response.data
-      })
-    }
-
-    $scope.adicionarContato = contato => {
-      contato.data = new Date()
-      $http
-        .post(`${baseURL}/contatos`, contato)
-        .then(response => {
-          delete $scope.contato
-          $scope.contatoForm.$setPristine()
-          carregarContatos()
+      const carregarContatos = () => {
+        contatosApi.getContatos().then(response => {
+          $scope.contatos = response.data
         })
-        .catch(response => {
-          $scope.message = "Acontece um problema: " + response.data
+      }
+
+      const carregarOperadoras = () => {
+        operadorasApi.getOperadoras().then(response => {
+          $scope.operadoras = response.data
         })
-    }
+      }
 
-    $scope.apagarContatos = contatos => {
-      $scope.contatos = contatos.filter(contato => {
-        if (!contato.selecionado) return contato
-      })
-    }
+      $scope.adicionarContato = contato => {
+        contato.serial = serialGenerator.generate()
+        contato.data = new Date()
 
-    $scope.isContatoSelecionado = contatos => {
-      return contatos.some(contato => {
-        return contato.selecionado
-      })
-    }
+        contatosApi
+          .saveContato(contato)
+          .then(response => {
+            delete $scope.contato
+            $scope.contatoForm.$setPristine()
+            carregarContatos()
+          })
+          .catch(response => {
+            $scope.message = "Aconteceu um problema: " + response.data
+          })
+      }
 
-    $scope.ordenarPor = campo => {
-      $scope.criterioDeOrdenacao = campo
-      $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao
-    }
+      $scope.apagarContatos = contatos => {
+        $scope.contatos = contatos.filter(contato => {
+          if (!contato.selecionado) return contato
+        })
+      }
 
-    carregarContatos()
-    carregarOperadoras()
-  })
+      $scope.isContatoSelecionado = contatos => {
+        return contatos.some(contato => {
+          return contato.selecionado
+        })
+      }
+
+      $scope.ordenarPor = campo => {
+        $scope.criterioDeOrdenacao = campo
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao
+      }
+
+      carregarContatos()
+      carregarOperadoras()
+    }
+  )
--- js/services/contatosApiService.js
+++ js/services/contatosApiService.js
@@ -1,1 +1,14 @@
-angular.module("listaTelefonica").factory
+angular.module("listaTelefonica").factory("contatosApi", ($http, config) => {
+  const _getContatos = () => {
+    return $http.get(`${config.baseUrl}/contatos`)
+  }
+
+  const _saveContato = contato => {
+    return $http.post(`${config.baseUrl}/contatos`, contato)
+  }
+
+  return {
+    getContatos: _getContatos,
+    saveContato: _saveContato
+  }
+})
